99966765124dd396a5492e525d5caff03d98b6a8
Pablo Guerreño <guerre.pablo.agustin@gmail.com>
2024-01-22 21:09:13 -0300

    mejorando responsive

--- src/Components/Skills/Skils.jsx
+++ src/Components/Skills/Skils.jsx
@@ -45,42 +45,46 @@ const Skills = () => {
       </div>
       <div className=" flex-grow flex items-center justify-center flex-row max-[900px]:flex-col">
         {/*Imagen*/}
-        <div className="m-8 flex  h-[40%] items-center justify-center ms:w-full w-[40%]">
+        <div className="m-8 flexitems-center justify-center ms:w-full ">
           <img
             src={imagen}
-            className="shadow-xl min-[400px]:w[80%] shadow-prymary min-[1300px]:ml-10"
+            className=" min-[400px]:w[80%]  min-[1300px]:ml-10"
             alt="imagen"
             style={{ width: "500px", height: "400px" }}
           />
         </div>
         {/*Habilidades*/}
-        <div className="flex flex-col items-center w-full w-[40%]  shadow-xl m-8 text-xl italic">
-          <div className="flex flex-row gap-8 text-lg">
-            <p className="text-center">Motores de bases de datos</p>
-            <div className="flex justify-center gap-4 w-full md:w-[80%] mx-auto">
-              <BiLogoMongodb className="text-5xl text-green-400" />
-              <SiMysql className="text-5xl text-orange-400" />
-              <BiLogoPostgresql className="text-5xl text-blue-400" />
-              <SiMicrosoftsqlserver className="text-5xl text-red-400" />
+        <div className="flex flex-col items-center w-full m-8 text-xl italic">
+          <div className="flex flex-col gap-4 text-lg">
+            <p className="text-center text-xl font-bold">
+              Motores de bases de datos
+            </p>
+            <div className="flex flex-wrap justify-center gap-4 w-full">
+              <BiLogoMongodb className="text-4xl md:text-5xl lg:text-6xl text-green-400" />
+              <SiMysql className="text-4xl md:text-5xl lg:text-6xl text-orange-400" />
+              <BiLogoPostgresql className="text-5xl md:text-6xl lg:text-7xl text-blue-400" />
+              <SiMicrosoftsqlserver className="text-5xl md:text-6xl lg:text-7xl text-red-400" />
             </div>
           </div>
           <br />
-          <div className="flex flex-row  text-lg">
-            <p className="text-center">Herramientas de desarrollo</p>
-            <div className="flex justify-center gap-4 w-full md:w-[80%] mx-auto">
-              <BiLogoHtml5 className="text-5xl text-orange-600" />
-              <IoLogoJavascript className="text-5xl text-yellow-400" />
-              <SiVite className="text-5xl text-violet-600" />
-              <BiLogoWordpress className="text-5xl text-violet-600" />
-              <SiNextdotjs className="text-5xl text-black" />
+          <div className="flex flex-wrap flex-col gap-4 text-lg">
+            <p className="text-center text-xl font-bold">
+              Herramientas de desarrollo
+            </p>
+            <div className="flex flex-wrap justify-center gap-4 w-full">
+              <BiLogoHtml5 className="text-5xl md:text-6xl lg:text-7xl text-orange-600" />
+              <IoLogoJavascript className="text-5xl md:text-6xl lg:text-7xl text-yellow-400" />
+              <SiVite className="text-5xl md:text-6xl lg:text-7xl text-violet-600" />
+              <BiLogoWordpress className="text-5xl md:text-6xl lg:text-7xl text-violet-600" />
+              <SiNextdotjs className="text-5xl md:text-6xl lg:text-7xl text-black" />
             </div>
           </div>
           <br />
-          <div className="flex flex-row gap-10 text-lg">
-            <p className="text-left -ml-40">Herramientas de CSS</p>
-            <div className="flex justify-center gap-4 w-full md:w-[80%] mx-auto">
-              <BiLogoCss3 className="text-5xl text-blue-700" />
-              <SiTailwindcss className="text-5xl text-blue-500" />
+          <div className="flex flex-col gap-4 text-lg ">
+            <p className="text-center text-xl font-bold">Herramientas de CSS</p>
+            <div className="flex flex-wrap justify-center gap-4 w-full">
+              <BiLogoCss3 className="text-5xl md:text-6xl lg:text-7xl text-blue-700" />
+              <SiTailwindcss className="text-5xl md:text-6xl lg:text-7xl text-blue-500" />
             </div>
           </div>
         </div>
