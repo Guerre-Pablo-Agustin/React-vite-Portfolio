fa04179da16d97591287fad99f8f130749a24ff7
Pablo Guerreño <guerre.pablo.agustin@gmail.com>
2024-02-01 15:25:58 -0300

    arreglos en css

--- src/Components/Skills/Skils.jsx
+++ src/Components/Skills/Skils.jsx
@@ -60,10 +60,10 @@ const Skills = () => {
               Motores de bases de datos
             </p>
             <div className="flex flex-wrap justify-center gap-4 w-full">
-              <BiLogoMongodb className="text-4xl md:text-5xl lg:text-6xl text-green-400" />
-              <SiMysql className="text-4xl md:text-5xl lg:text-6xl text-orange-400" />
-              <BiLogoPostgresql className="text-5xl md:text-6xl lg:text-7xl text-blue-400" />
-              <SiMicrosoftsqlserver className="text-5xl md:text-6xl lg:text-7xl text-red-400" />
+              <BiLogoMongodb className="text-4xl md:text-5xl lg:text-6xl bg-primary p-2 rounded-md text-white" />
+              <SiMysql className="text-2xl md:text-5xl lg:text-6xl bg-primary p-2 rounded-md text-white" />
+              <BiLogoPostgresql className="text-5xl md:text-6xl lg:text-6xl bg-primary p-2 rounded-md text-white" />
+              <SiMicrosoftsqlserver className="text-5xl md:text-6xl lg:text-6xl bg-primary p-2 rounded-md text-white" />
             </div>
           </div>
           <br />
@@ -72,21 +72,17 @@ const Skills = () => {
               Herramientas de desarrollo
             </p>
             <div className="flex flex-wrap justify-center gap-4 w-full">
-              <BiLogoHtml5 className="text-5xl md:text-6xl lg:text-7xl text-orange-600" />
-              <IoLogoJavascript className="text-5xl md:text-6xl lg:text-7xl text-yellow-400" />
-              <SiVite className="text-5xl md:text-6xl lg:text-7xl text-violet-600" />
-              <BiLogoWordpress className="text-5xl md:text-6xl lg:text-7xl text-violet-600" />
-              <SiNextdotjs className="text-5xl md:text-6xl lg:text-7xl text-black" />
+              <BiLogoHtml5 className="text-5xl md:text-6xl  bg-primary p-2 rounded-md text-white" />
+              <IoLogoJavascript className="text-5xl md:text-6xl  bg-primary p-2 rounded-md text-white" />
+              <SiVite className="text-5xl md:text-6xl  bg-primary p-2 rounded-md text-white" />
+              <BiLogoWordpress className="text-5xl md:text-6xl  bg-primary p-2 rounded-md text-white" />
+              <SiNextdotjs className="text-5xl md:text-6xl  bg-primary p-2 rounded-md text-white" />
+              <BiLogoCss3 className="text-5xl md:text-6xl  bg-primary p-2 rounded-md text-white" />
+              <SiTailwindcss className="text-5xl md:text-6xl  bg-primary p-2 rounded-md text-white" />
             </div>
           </div>
           <br />
-          <div className="flex flex-col gap-4 text-lg ">
-            <p className="text-center text-xl font-bold">Herramientas de CSS</p>
-            <div className="flex flex-wrap justify-center gap-4 w-full">
-              <BiLogoCss3 className="text-5xl md:text-6xl lg:text-7xl text-blue-700" />
-              <SiTailwindcss className="text-5xl md:text-6xl lg:text-7xl text-blue-500" />
-            </div>
-          </div>
+          
         </div>
       </div>
     </section>
